62e01538a198d8c52169321d008b73708714bf1d
gwvsol <gwvsol@gmail.com>
2018-11-22 12:59:57 +0200

    added file config

---
+++ config.py
@@ -0,0 +1,6 @@
+from heat_control import config
+
+config['IP'] = None                        #Дефолтный IP адрес
+config['ssid'] = 'w2234'                   #SSID для подключения к WiFi
+config['wf_pass'] = 'Fedex##54'            #Пароль для подключения к WiFi
+config['internet_outage'] = True           #Интернет отключен(значение True)
