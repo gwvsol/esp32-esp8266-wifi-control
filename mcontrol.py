39c7691f39b93ad6d7f092954560eac50c7a5387
gwvsol <gwvsol@gmail.com>
2019-01-02 16:18:43 +0200

    fixed connection presence control errors

--- mcontrol.py
+++ mcontrol.py
@@ -1,6 +1,6 @@
 import gc
 import uasyncio as asyncio
-from machine import Pin
+from machine import Pin, freq
 from wificonnect import WiFiControl
 
 #=======================================================================
@@ -10,35 +10,51 @@ class Main(WiFiControl):
         self.DEBUG = True                               #Режим отладки, делаем программу разговорчивой
         self.wifi_led = Pin(2, Pin.OUT, value = 1)      #Pin2, светодиод на плате контроллера
 
+
         loop = asyncio.get_event_loop()
         loop.create_task(self._heartbeat())             #Индикация подключения WiFi
     
     #Индикация подключения WiFi
     async def _heartbeat(self):
         while True:
-            if self.config['internet_outage']:
+            if self.config['internet_outage'] == True:
                 self.wifi_led(not self.wifi_led())      #Быстрое мигание, если соединение отсутствует
-                await asyncio.sleep_ms(200) 
-            else:
+                await asyncio.sleep_ms(200)
+            elif self.config['internet_outage'] == False:
                 self.wifi_led(0)                        #Редкое мигание при подключении
+                await asyncio.sleep_ms(50)
+                self.wifi_led(1)
+                await asyncio.sleep_ms(5000)
+            else:
+                self.wifi_led(0)                        #Два быстрых миганиения при AP Mode
+                await asyncio.sleep_ms(50)
+                self.wifi_led(1)
+                await asyncio.sleep_ms(50)
+                self.wifi_led(0)
                 await asyncio.sleep_ms(50)
                 self.wifi_led(1)
                 await asyncio.sleep_ms(5000)
 
 
-    async def _run_main_loop(self):                     #Бесконечный цикл
-        mins = 0
+    async def _run_main_loop(self):                                     #Бесконечный цикл
         while True:
-            gc.collect()                                #Очищаем RAM
+            self.config['MemFree'] = str(round(gc.mem_free()/1024, 2))
+            self.config['MemAvailab'] = str(round(gc.mem_alloc()/1024, 2))
+            self.config['FREQ'] = str(freq()/1000000)
+            wifi = 'connected' if not self.config['internet_outage'] else 'disconnected'
+            gc.collect()                                                #Очищаем RAM
             try:
-                self.dprint('Uptime:', str(mins)+' min')
-                self.dprint('Not WiFi:', self.config['internet_outage'])
+                self.dprint('################# DEBUG MESSAGE ##########################')
+                self.dprint('Uptime:', str(self.config['Uptime'])+' min')
+                self.dprint('WiFi:', wifi)
                 self.dprint('IP:', self.config['IP'])
-                self.dprint('MemFree:', str(round(gc.mem_free()/1024, 2))+' Kb')
-                self.dprint('MemAlloc:', str(round(gc.mem_alloc()/1024, 2))+' Kb')
+                self.dprint('MemFree:', '{}Kb'.format(self.config['MemFree']))
+                self.dprint('MemAvailab:', '{}Kb'.format(self.config['MemAvailab']))
+                self.dprint('FREQ:', '{}MHz'.format(self.config['FREQ']))
+                self.dprint('################# DEBUG MESSAGE END ######################')
             except Exception as e:
                 self.dprint('Exception occurred: ', e)
-            mins += 1
+            self.config['Uptime'] += 1
             await asyncio.sleep(60)
 
 
--- wificonnect.py
+++ wificonnect.py
@@ -3,18 +3,25 @@ import uasyncio as asyncio
 gc.collect()                                #Очищаем RAM
 
 config = {}                                 #Основное хранилище настроек
-config['MODE_WiFi'] = 'ST'
+config['MODE_WiFi'] = 'ST'                  #Режим работы WiFi AP или ST
 config['ssid'] = 'w2234'                    #SSID для подключения к WiFi
 config['wf_pass'] = 'Fedex##54'             #Пароль для подключения к WiFi
 config['IP'] = None                         #Дефолтный IP адрес
 config['internet_outage'] = True            #Интернет отключен(значение True)
+config['Uptime'] = 0                        #Время работы контроллера
 
-#=======================================================================
+
 #Базовый класс
 class WiFiBase:
     DEBUG = False
     def __init__(self, config):
         self.config = config
+        if self.config['MODE_WiFi'] == 'AP':
+            self._ap_if = network.WLAN(network.AP_IF)
+            self.config['WIFI'] = self._ap_if
+        elif self.config['MODE_WiFi'] == 'ST':
+            self._sta_if = network.WLAN(network.STA_IF)
+            self.config['WIFI'] = self._sta_if
 
 
     #Выводим отладочные сообщения
@@ -23,103 +30,120 @@ class WiFiBase:
             print(*args)
 
 
-    async def ping(self):
-    # Проверяем есть ли соединение с интернетом
-        try:
-            addr = socket.getaddrinfo(self.config['host'], 80)[0][-1]
-            self.dprint('Ping OK!')
-            return True
-        except OSError:
-            self.dprint('Ping ERROR!')
-            return False
-
-
+    #Настройка для режима Точка доступа и подключения к сети WiFi
     def _con(self):
-        self.wf.active(True)
-        network.phy_mode(1) # network.phy_mode = MODE_11B
-        self.wf.connect(self.config['ssid'], self.config['wf_pass'])
+        if self.config['MODE_WiFi'] == 'AP':
+            self.config['WIFI'].active(True)
+            #Устанавливаем SSID и пароль для подключения к Точке доступа
+            self.config['WIFI'].config(essid=self.config['ssid'], password=self.config['wf_pass'])
+            #Устанавливаем статический IP адрес, шлюз, dns
+            self.config['WIFI'].ifconfig(self.config['WIFI_AP'])
+        elif self.config['MODE_WiFi'] == 'ST':
+            self.config['WIFI'].active(True)
+            network.phy_mode(1) # network.phy_mode = MODE_11B
+            #Подключаемся к WiFi сети
+            self.config['WIFI'].connect(self.config['ssid'], self.config['wf_pass'])
 
-
+    
+    #Выводим сообщения об ошибках соединения
     def _error_con(self):
         #Соединение не установлено...
-        if self.wf.status() == network.STAT_CONNECT_FAIL:
+        if self.config['WIFI'].status() == network.STAT_CONNECT_FAIL:
             self.dprint('WiFi: Failed due to other problems')
         #Соединение не установлено, причина не найдена точка доступа
-        if self.wf.status() == network.STAT_NO_AP_FOUND:
+        if self.config['WIFI'].status() == network.STAT_NO_AP_FOUND:
             self.dprint('WiFi: Failed because no access point replied')
         #Соединение не установлено, не верный пароль
-        if self.wf.status() == network.STAT_WRONG_PASSWORD:
+        if self.config['WIFI'].status() == network.STAT_WRONG_PASSWORD:
             self.dprint('WiFi: Failed due to incorrect password')
 
 
+    #Подключение к сети WiFi или поднятие точки доступа
     async def connect_wf(self):
-        self.dprint('Connecting to WiFi...')
-        self._con()
-        if self.wf.status() == network.STAT_CONNECTING:
-            self.dprint('WiFi: Waiting for connection to...')
-        # Задержка на соединение, если не успешно, будет выдана одна из ошибок
-        # Выполнение условия проверяем каждую секунду, задержка для получения IP адреса от DHCP
-        while self.wf.status() == network.STAT_CONNECTING:
-            await asyncio.sleep(1)
-        #Соединение успешно установлено
-        if self.wf.status() == network.STAT_GOT_IP:
-            self.dprint('WiFi: Connection successfully!')
-            self.config['IP'] = self.wf.ifconfig()[0]
-            self.dprint('WiFi:', self.config['IP'])
-            self.config['internet_outage'] = False
-        if not self.wf.isconnected():
-            self.config['internet_outage'] = True
-            self.dprint('WiFi: Connection unsuccessfully!')
-        self._error_con()
+        if self.config['MODE_WiFi'] == 'AP': #Если точка доступа
+            self.dprint('WiFi AP Mode!')
+            self._con() #Настройка для режима Точка доступа и подключения к сети WiFi
+            if self.config['WIFI'].status() == -1:
+                self.dprint('WiFi: AP Mode OK!')
+                self.config['IP'] = self.config['WIFI'].ifconfig()[0]
+                self.dprint('WiFi:', self.config['IP'])
+                self.config['internet_outage'] = 'AP'
+        elif self.config['MODE_WiFi'] == 'ST': #Если подключаемся к сети
+            self.dprint('Connecting to WiFi...')
+            self._con() #Настройка для режима Точка доступа и подключения к сети WiFi
+            if self.config['WIFI'].status() == network.STAT_CONNECTING:
+                self.dprint('WiFi: Waiting for connection to...')
+            # Задержка на соединение, если не успешно, будет выдана одна из ошибок
+            # Выполнение условия проверяем каждую секунду, задержка для получения IP адреса от DHCP
+            while self.config['WIFI'].status() == network.STAT_CONNECTING:
+                await asyncio.sleep(1)
+            #Соединение успешно установлено
+            if self.config['WIFI'].status() == network.STAT_GOT_IP:
+                self.dprint('WiFi: Connection successfully!')
+                self.config['IP'] = self.config['WIFI'].ifconfig()[0]
+                self.dprint('WiFi:', self.config['IP'])
+                self.config['internet_outage'] = False #Сообщаем, что соединение успешно установлено
+            #Если соединение по каким-то причинам не установлено
+            if not self.config['WIFI'].isconnected():
+                self.config['internet_outage'] = True #Сообщаем, что соединение не установлено
+                self.dprint('WiFi: Connection unsuccessfully!')
+            self._error_con() #Выводим сообщения, о причинах отсутствия соединения
 
 
+    #Переподключаемся к сети WiFi
     async def reconnect(self):
         self.dprint('Reconnecting to WiFi...')
-        self.config['IP'] = self.wf.ifconfig()[0]
-        self.wf.disconnect()
+        #Сбрасываем IP адрес к виду 0.0.0.0
+        self.config['IP'] = self.config['WIFI'].ifconfig()[0]
+        #Разрываем соединение, если они не разорвано
+        self.config['WIFI'].disconnect()
         await asyncio.sleep(1)
-        self._con()
-        while self.wf.status() == network.STAT_CONNECTING:
+        self._con() #Настройка для режима Точка доступа и подключения к сети WiFi
+        # Задержка на соединение, если не успешно, будет выдана одна из ошибок
+        # Выполнение условия проверяем каждые 20 милисекунд, задержка для получения IP адреса от DHCP
+        while self.config['WIFI'].status() == network.STAT_CONNECTING:
             await asyncio.sleep_ms(20)
-        if self.wf.status() == network.STAT_GOT_IP:
-            self.config['IP'] = self.wf.ifconfig()[0]
-            self.config['internet_outage'] = False
+        #Если соединение установлено
+        if self.config['WIFI'].status() == network.STAT_GOT_IP:
+            #Сохраняем новый IP адрес
+            self.config['IP'] = self.config['WIFI'].ifconfig()[0]
+            self.config['internet_outage'] = False #Сообщаем, что соединение успешно установлено
             self.dprint('WiFi: Reconnecting successfully!')
             self.dprint('WiFi:', self.config['IP'])
-        self._error_con()
-        if not self.wf.isconnected():
-            self.config['internet_outage'] = True
+        self._error_con() #Выводим сообщения, о причинах отсутствия соединения
+        #Если по какой-то причине соединение не установлено
+        if not self.config['WIFI'].isconnected():
+            self.config['internet_outage'] = True #Сообщаем, что соединение не установлено
             self.dprint('WiFi: Reconnecting unsuccessfully!')
         await asyncio.sleep(1)
 
 
-#=======================================================================
 class WiFiControl(WiFiBase):
     def __init__(self):
         super().__init__(config)
-        self._sta_if = network.WLAN(network.STA_IF)
-        self.wf = self._sta_if
 
 
     #Проверка соединения с Интернетом
     async def _check_wf(self):
         while True:
-            if not self.config['internet_outage']:
-                if self.wf.status() == network.STAT_GOT_IP:
+            if not self.config['internet_outage']:                      #Если оединение установлено
+                if self.config['WIFI'].status() == network.STAT_GOT_IP: #Проверяем наличие соединения
                     await asyncio.sleep(1)
-                else:
+                else:                                                   #Если соединение отсутсвует или оборвано
                     await asyncio.sleep(1)
-                    self.config['internet_outage'] = True
-            else:
+                    self.config['internet_outage'] = True               #Сообщаем, что соединение оборвано
+            else:                                                       #Если соединение отсутсвует
                 await asyncio.sleep(1)
-                await self.reconnect()
+                await self.reconnect()                                  #Переподключаемся
         await asyncio.sleep(1)
-        gc.collect()                                   #Очищаем RAM
+        gc.collect() 
 
 
-    #Подключаемся к WiFi
+    #Подключаемся к WiFi или поднимаем точку доступа
     async def connect(self):
-        await self.connect_wf()
-        
-        loop = asyncio.get_event_loop()
-        loop.create_task(self._check_wf())
+        await self.connect_wf()                                         #Подключение или точка доступа, зависит от настройки
+        if self.config['MODE_WiFi'] == 'ST':
+            loop = asyncio.get_event_loop()
+            loop.create_task(self._check_wf())
+        elif self.config['MODE_WiFi'] == 'AP':
+            gc.collect() 
