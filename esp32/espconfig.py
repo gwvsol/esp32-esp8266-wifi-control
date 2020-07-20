4471032b041f34fc828b83abd54cc20003b9edf3
gwvsol <gwvsol@gmail.com>
2020-07-20 23:24:59 +0300

    добавлены новые переменные для работы сети wifi

--- esp32/espconfig.py
+++ esp32/espconfig.py
@@ -1,7 +1,9 @@
-config = {}                  # Основное хранилище настроек
-config['debug'] = True       # Режим отладки, вывод всех сообщений
-config['uptime'] = 0         # Время работы контроллера
-config['memfree'] = 0        # Свободная память
-config['memavalable'] = 0    # Доступная помять
-config['freq'] = 0           # Частота работы ядра процессора
+config = {}                     # Основное хранилище настроек
+config['debug']         = True  # Режим отладки, вывод всех сообщений
+config['uptime']        = 0     # Время работы контроллера
+config['memfree']       = 0     # Свободная память
+config['memavalable']   = 0     # Доступная помять
+config['freq']          = 0     # Частота работы ядра процессора
+config['connect']       = False # False = Подключения нет, True = Подключение к сети WiFi
+config['wifiMode']      = False # False = AP, True = ST
 
