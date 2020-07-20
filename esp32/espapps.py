091b6810952f1ef6998a9f990898726d41c80703
gwvsol <gwvsol@gmail.com>
2020-07-20 23:26:53 +0300

    изменения в основном методе приложения

--- esp32/espapps.py
+++ esp32/espapps.py
@@ -2,34 +2,36 @@ import gc
 import uasyncio as asyncio
 from machine import Pin, freq
 from espconfig import *
+from esputils import dprint
 
 
 class MainApps(object):
 
     def __init__(self):
-        self.debug = config['debug']
-    
-    #Выводим отладочные сообщения
-    def dprint(self, *args):
-        if self.debug: print(*args)
+        self.dprint      = dprint
+        self.debug       = config['debug']
+        self.memfree     = config['memfree']
+        self.memavalable = config['memavalable']
+        self.freq        = config['freq']
+        self.uptime      = config['uptime']
 
     
     async def _run_main_loop(self):                                     #Бесконечный цикл
         while True:
-            config['memfree'] = str(round(gc.mem_free()/1024, 2))
-            config['memavalable'] = str(round(gc.mem_alloc()/1024, 2))
-            config['freq'] = str(freq()/1000000)
+            self.memfree = str(round(gc.mem_free()/1024, 2))
+            self.memavalable = str(round(gc.mem_alloc()/1024, 2))
+            self.freq = str(freq()/1000000)
             gc.collect()                                                #Очищаем RAM
             try:
                 self.dprint('################# DEBUG MESSAGE ##########################')
-                self.dprint('Uptime:', str(config['uptime'])+' min')
-                self.dprint('MemFree:', '{}Kb'.format(config['memfree']))
-                self.dprint('MemAvailab:', '{}Kb'.format(config['memavalable']))
-                self.dprint('FREQ:', '{}MHz'.format(config['freq']))
+                self.dprint('Uptime:', str(self.uptime)+' min')
+                self.dprint('MemFree:', '{}Kb'.format(self.memfree))
+                self.dprint('MemAvailab:', '{}Kb'.format(self.memavalable))
+                self.dprint('FREQ:', '{}MHz'.format(self.freq))
                 self.dprint('################# DEBUG MESSAGE END ######################')
             except Exception as err:
                 self.dprint('Exception occurred: ', err)
-            config['uptime'] += 1
+            self.uptime += 1
             await asyncio.sleep(60)
 
 
