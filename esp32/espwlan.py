fb903a3ff11cbeecc59cbf23c117db0f34398b8b
gwvsol <gwvsol@gmail.com>
2020-07-23 00:24:42 +0300

    добавлены два метода для настроки подключения к точке доступа

--- esp32/espwlan.py
+++ esp32/espwlan.py
@@ -11,5 +11,35 @@ class WiFiConnect(object):
         self.mode       = config['wifiMode']                        # False = AP, True = ST
         self.connect    = config['connect']                         # False = Подключения нет, True = Подключение к сети WiFi
         self.debug      = config['debug']
-        if self.wifi: self.station = network.WLAN(network.STA_IF)   # ST Mode
-        else: self.wifi = network.WLAN(network.AP_IF)               # AP Mode
+        self.station    = config['mode']
+        self.ip         = config['ip']
+
+    
+    #Подключение к сети WiFi или поднятие точки доступа
+    async def setWlan(self):
+        """Метод для активации режимов работы WiFi модуля"""
+        if self.station: 
+            self.wifi = network.WLAN(network.STA_IF)   # ST Mode
+            self.ssid, self.passwd = config['stssid'], config['stpasswd']
+        else: 
+            self.wifi = network.WLAN(network.AP_IF)    # AP Mode
+            self.ssid, self.passwd = config['apssid'], config['appasswd']
+        self.wifi.active(True)       # activate the interface
+        if self.station: await setSTMode()
+        else: pass
+
+
+    async def setSTMode(self):
+        """Метод для подключения к точке доступа"""
+        network.phy_mode(network.MODE_11B)      # network.phy_mode = MODE_11B
+        if not self.wifi.isconnected():
+            self.wifi.connect(self.ssid, self.passwd)
+        while self.wifi.status() == network.STAT_CONNECTING:
+            self.dprint('Connecting to WiFi...')
+            await asyncio.sleep(1)
+        if self.wifi.status() == network.STAT_GOT_IP:
+            self.dprint('WiFi: Connection successfully!')
+            self.ip = self.wifi.ifconfig()[0]
+            self.connect = True                 # соединение успешно установлено
+            self.dprint('WiFi:', self.ip)
+
