8b4510fd0cb6a0af60e4b63028b053ea349f0fc7
gwvsol <gwvsol@gmail.com>
2020-08-16 22:40:36 +0300

    настройка файла конфигурации

--- esp32/espconfig.py
+++ esp32/espconfig.py
@@ -1,8 +1,13 @@
 config = {}                             # Основное хранилище настроек
-config['debug']         = True          # Режим отладки, вывод всех сообщений
-config['stssid']        = "wSG3500"     # Имя точки доступа для подключения
-config['stpasswd']      = "Fedex##45%"  # Пароль от точки доступа
-config['apssid']        = "esp32AP"     # Имя точки доступа контроллера
-config['appasswd']      = "roottoor"    # Пароль точки доступа контроллера
-config['mode']          = True          # False = AP, True = ST
+config['debug']          = True          # Режим отладки, вывод всех сообщений
+config['stssid']         = "GWVSOL"       # Имя точки доступа для подключения
+#config['stssid']         = "wSG3500"     # Имя точки доступа для подключения
+config['stpasswd']       = "fec0569cfaee" # Пароль от точки доступа
+#config['stpasswd']       = "Fedex##45%"  # Пароль от точки доступа
+config['apssid']         = "esp32AP"     # Имя точки доступа контроллера
+config['appasswd']       = "roottoor"    # Пароль точки доступа контроллера
+config['mode']           = True          # False = AP, True = ST
+#config['mode']           = False         # False = AP, True = ST
+config['wifiLedPin']     = 13            # Светодиод на плате подключен к 13 контакту
+config['wifiLedDefault'] = 0             # Катод светодиода подключен на землю
 
