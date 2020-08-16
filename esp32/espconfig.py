02e329b1684593e755c3618c74f373fb19f51194
gwvsol <gwvsol@gmail.com>
2020-08-16 23:20:34 +0300

    изменение для работы с wifi роутером

--- esp32/espconfig.py
+++ esp32/espconfig.py
@@ -1,9 +1,9 @@
 config = {}                             # Основное хранилище настроек
 config['debug']          = True          # Режим отладки, вывод всех сообщений
-config['stssid']         = "GWVSOL"       # Имя точки доступа для подключения
-#config['stssid']         = "wSG3500"     # Имя точки доступа для подключения
-config['stpasswd']       = "fec0569cfaee" # Пароль от точки доступа
-#config['stpasswd']       = "Fedex##45%"  # Пароль от точки доступа
+#config['stssid']         = "GWVSOL"       # Имя точки доступа для подключения
+config['stssid']         = "wSG3500"     # Имя точки доступа для подключения
+#config['stpasswd']       = "fec0569cfaee" # Пароль от точки доступа
+config['stpasswd']       = "Fedex##45%"  # Пароль от точки доступа
 config['apssid']         = "esp32AP"     # Имя точки доступа контроллера
 config['appasswd']       = "roottoor"    # Пароль точки доступа контроллера
 config['mode']           = True          # False = AP, True = ST
