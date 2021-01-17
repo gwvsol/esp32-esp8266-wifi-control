5bf12b6dbcb622967de820422bbcc0960e7b7899
gwvsol <gwvsol@gmail.com>
2021-01-17 21:31:07 +0300

    обновление логирования

--- esp8266/espapps.py
+++ esp8266/espapps.py
@@ -2,7 +2,7 @@ import gc
 import uasyncio as asyncio
 from machine import Pin, freq
 from espconfig import *
-from esputils import dprint
+from esputils import log
 from espwlan import WiFiConnect
 
 """Здесь реализуется основное приложение
@@ -12,39 +12,31 @@ from espwlan import WiFiConnect
 class MainApps(object):
 
     def __init__(self):
-        self.dprint          = dprint
+        self.log             = log
         self.debug           = config['debug']
         self.mode            = config['mode']
         self.ledBoard        = config['wifiLedPin']
         self.ledBoardDefault = config['wifiLedDefault']
-        self.uptime          = 0                                   # Время работы контроллера
-        self.wifiLed         = Pin(self.ledBoard, Pin.OUT, value = self.ledBoardDefault) # Cветодиод для индикации работы WiFi
-        self.wifi            = WiFiConnect(led=self.wifiLed)       # Настрока сети
+        self.wifiLed         = Pin(self.ledBoard, Pin.OUT, value = self.ledBoardDefault)    # Cветодиод для индикации работы WiFi
+        self.wifi            = WiFiConnect(led=self.wifiLed)                                # Настрока сети
     
     async def main_loop(self):
         """Метод для вывода служебной отладочной информации"""
-        while True:                                                # Бесконечный цикл
-            self.ip          = self.wifi.ip                        # IP адрес полученный c DHCP роутера
-            self.connect     = self.wifi.connect                   # False = Подключения нет, True = Подключение к сети WiFi
-            self.memfree = str(round(gc.mem_free()/1024, 2))       # Свободная память
-            self.memavalable = str(round(gc.mem_alloc()/1024, 2))  # Доступная помять
-            self.freq = str(freq()/1000000)                        # Частота работы ядра процессора
+        while True:
             mode = 'Station' if self.mode else 'Access Point'      # Подготовка информации о режиме работы WiFi модуля
-            connect = 'Connect' if self.connect else 'Disconnect'  # Подготовка информации о соединении
+            # connect = 'Connect' if self.connect else 'Disconnect'  # Подготовка информации о соединении
             gc.collect()                                           # Очищаем RAM
             try:
-                self.dprint('################# DEBUG MESSAGE ##########################')
-                self.dprint('Uptime:', str(self.uptime)+' min')
-                self.dprint('MemFree:', '{}Kb'.format(self.memfree))
-                self.dprint('MemAvailab:', '{}Kb'.format(self.memavalable))
-                self.dprint('FREQ:', '{}MHz'.format(self.freq))
-                self.dprint('WiFi Mode:', mode)
-                self.dprint('WiFi:', connect)
-                self.dprint('IP:', '{}'.format(self.ip))
-                self.dprint('################# DEBUG MESSAGE END ######################')
+                # self.log('################# DEBUG MESSAGE #################')
+                self.log('MainApps => MemFree:', '{}Kb'.format(round(gc.mem_free()/1024, 2)))       # Свободная память
+                self.log('MainApps => MemAllocated:', '{}Kb'.format(round(gc.mem_alloc()/1024, 2))) # Доступная помять
+                # self.log('FREQ:', '{}MHz'.format(freq()/1000000))
+                self.log('MainApps => WiFi Mode:', mode)
+                # self.log('WiFi:', connect)
+                self.log('MainApps => IP:', '{}'.format(self.wifi.ip))                              # IP адрес ESP8266
+                # self.log('################# DEBUG MESSAGE END #################')
             except Exception as err:
-                self.dprint('Exception occurred: ', err)
-            self.uptime += 1
+                self.log('MainApps => Exception occurred: ', err)
             await asyncio.sleep(60)
 
 
@@ -54,7 +46,7 @@ class MainApps(object):
                 await self.wifi.setWlan()                           # Включение настройки сети
                 await self.main_loop()                              # Вывод служебной информации
             except Exception as err:
-                self.dprint('Global communication failure: ', err)
+                self.log('MainApps => Global communication failure: ', err)
                 await asyncio.sleep(20)
 
 
@@ -62,3 +54,4 @@ gc.collect()                                                        # Очища
 def_main = MainApps()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(def_main.main())
+
