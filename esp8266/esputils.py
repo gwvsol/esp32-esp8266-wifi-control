be9a8e1561584df7d7db97b99b720e53e87ec28a
gwvsol <gwvsol@gmail.com>
2021-01-17 21:30:52 +0300

    обновление логирования

--- esp8266/esputils.py
+++ esp8266/esputils.py
@@ -1,9 +1,18 @@
+from time import localtime
 from espconfig import *
 
 """Здесь реализованы методы для использования во всем проекте"""
 
 debug = config['debug']
 
-#Выводим отладочные сообщения
-def dprint(*args):
-    if debug: print(*args) 
+def log(*args):
+    """Метод для логирования данных приложения"""
+    local = localtime()
+    year = local[0]
+    month = local[1] if local[1] >= 10 else '0{}'.format(local[1])
+    day = local[2] if local[2] >= 10 else '0{}'.format(local[2])
+    hour = local[3] if local[3] >= 10 else '0{}'.format(local[3])
+    minute = local[4] if local[4] >= 10 else '0{}'.format(local[4])
+    second = local[5] if local[5] >= 10 else '0{}'.format(local[5])
+    if debug: print('{}-{}-{} {}:{}:{} #'.format(year, month, day, hour, minute, second), *args)
+
